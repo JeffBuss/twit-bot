f4123f2659062dab0a0f80a4e0516c644a1f8b18
JeffBuss <turbocapslock@gmail.com>
2017-09-14 17:11:59 -0600

    Retweet bot working

---
+++ config.jsx
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'KEBf5frVeLIkTWEoVeCYTZAbB',
+  consumer_secret: 'nV8JXEishiyTR3feRSuWw59XubhqJnQztTjP3QTghf2dTruZtF',
+  access_token: '908449692618391552-pz7U62fsXvCf6UVc90HwjbhG5oHwOfF',
+  access_token_secret: 'qxHfeSu0UqtAa6MjIa3zfF5L9tfyRxHdufK0PavxhwLG6'
+}
